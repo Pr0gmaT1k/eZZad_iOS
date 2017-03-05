3255a09688af860328c469b87f78bf5df5694c44
Pr0gmaT1k <julien.cloupet@gmail.com>
2017-03-06 00:58:17 +0100

    Empty Project

---
+++ eZZad_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eZZad_iOS
+//
+//  Created by AzerTy on 06/03/2017.
+//  Copyright © 2017 Pr0gmaT1k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
