6829e3dee164edf2dd8359ea26dbe771ae57a941
Pr0gmaT1k <julien.cloupet@gmail.com>
2017-03-13 23:41:23 +0100

    Updated submodule eZZad_Common

---
+++ eZZad_iOS/MBTileOverlay.swift
@@ -0,0 +1,13 @@
+//
+//  MBTileOverlay.swift
+//  eZZad_iOS
+//
+//  Created by AzerTy on 13/03/2017.
+//  Copyright © 2017 Pr0gmaT1k. All rights reserved.
+//
+
+import UIKit
+
+class MBTileOverlay: MKTileOverlay {
+
+}
