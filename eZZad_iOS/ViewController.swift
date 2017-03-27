8ba3928a13ac17ae4bc949db1af2d7899e939700
Pr0gmaT1k <azerty@pr0gmat1k.com>
2017-03-27 19:14:03 +0200

    Documents and reorganise file in prevision of the futur evolutions

--- eZZad_iOS/ViewController.swift
+++ eZZad_iOS/ViewController.swift
@@ -8,37 +8,58 @@
 
 import UIKit
 import MapKit
+import GEOSwift
 
 //bbox -1.815,47.3169,-1.6306,47.3932
 
 class ViewController: UIViewController, MKMapViewDelegate {
+  //
   // Mark: IBoutlet
   @IBOutlet private weak var mapView: MKMapView!
   
+  //
   // Mark: public func
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupMap()
+    setupDB()
+    setupGeoJSON()
+  }
+  
+  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    guard let tileOverlay = overlay as? MKTileOverlay else { return MKOverlayRenderer() }
+    return MKTileOverlayRenderer(tileOverlay: tileOverlay)
+  }
+  
+  //
+  // Mark:- private func
+  /** Center map */
+  private func setupMap() {
     self.mapView.delegate = self
-    
-    //Center map
     let center = CLLocationCoordinate2D(latitude: 47.346471, longitude: -1.720943)
     let viewRegion : MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(center, 4000, 4000);
     let adjustedRegion : MKCoordinateRegion = self.mapView.regionThatFits(viewRegion)
     self.mapView.setRegion(adjustedRegion ,animated:true)
     self.mapView.showsUserLocation = true
-    
-    // Initialise DB
+  }
+  
+  /** Load GeoJSON From local file */
+  private func setupGeoJSON() {
+    if let geoJSONURL = Bundle.main.url(forResource: "AirportPerimeter", withExtension: ".geojson"),
+      let geometries = try? Geometry.fromGeoJSON(geoJSONURL),
+      let airportPerimeterPolygon = geometries?[0] as? Polygon,
+      let airportPerimeterOverlay = airportPerimeterPolygon.mapShape() as? MKOverlay {
+      self.mapView.add(airportPerimeterOverlay)
+    }
+  }
+  
+  /** Initialise Map DB from MBTiles File. */
+  private func setupDB() {
     if let dbPath = Bundle.main.url(forResource: "zad", withExtension: "mbtiles")?.path {
       let overlay = MBtilesOverlay(dbPath: dbPath)
       overlay.canReplaceMapContent = true
       mapView.add(overlay, level: .aboveLabels)
     }
-  }
-  
-  
-  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-    guard let tileOverlay = overlay as? MKTileOverlay else { return MKOverlayRenderer() }
-    return MKTileOverlayRenderer(tileOverlay: tileOverlay)
   }
 }
 
